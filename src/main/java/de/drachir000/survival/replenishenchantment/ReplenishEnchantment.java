24146c4982cf0f5f552c67b31e2bc756b3438510
Drachir000 <mra.berginski@gmail.com>
2023-01-01 13:02:45 +0100

    Initial commit

---
+++ src/main/java/de/drachir000/survival/replenishenchantment/ReplenishEnchantment.java
@@ -0,0 +1,18 @@
+package de.drachir000.survival.replenishenchantment;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ReplenishEnchantment extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
